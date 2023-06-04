21bad96b9f31506ea5215ee0d0ae3fed829a882c
Ben Gumaan <ben.gumaan@gmail.com>
2023-06-04 17:08:12 +0300

    Added new files

---
+++ employees.js
@@ -0,0 +1,57 @@
+
+export let employees = [
+    {
+        "id": 1,
+        "name": "David William",
+        "date": "May 2021",
+        "image": "images/user1.jpg",
+        "location": "USA",
+        "profession": "Dentist",
+        "rate": 4.6
+    },
+    {
+        "id": 2,
+        "name": "Robert Charles",
+        "date": "March 2022",
+        "image": "images/user2.jpg",
+        "location": "UK",
+        "profession": "Trader",
+        "rate": 3.9
+    },
+    {
+        "id": 3,
+        "name": "James Matthew",
+        "date": "Feb 2020",
+        "image": "images/user3.jpg",
+        "location": "Australia",
+        "profession": "Doctor",
+        "rate": 4.2
+    },
+    {
+        "id": 4,
+        "name": "Michael Daniel",
+        "date": "July 2023",
+        "image": "images/user4.jpg",
+        "location": "Canada",
+        "profession": "Teacher",
+        "rate": 5.0
+    },
+    {
+        "id": 5,
+        "name": "Donald Steven",
+        "date": "Jan 2023",
+        "image": "images/user5.jpg",
+        "location": "Netherlands",
+        "profession": "Designer",
+        "rate": 2.7
+    },
+    {
+        "id": 6,
+        "name": "Kevin Edward",
+        "date": "Dec 2022",
+        "image": "images/user6.jpg",
+        "location": "India",
+        "profession": "Developer",
+        "rate": 4.8
+    }
+  ];
